c2586799bda14e71dbb2a9b0d2c1417ebec22737
daniel <daniel@diamand.org>
2025-02-04 11:17:01 +0000

    Add gesture frame capturing distance and all gesture data

--- GestureFrame.py
+++ GestureFrame.py
@@ -3,66 +3,65 @@ import numpy as np
 MAX_HAND_DIST = 5
 
 class GestureFrame:
-    def __init__(self, multiLandmarks):
-        self.noOfHands = len(multiLandmarks)
-
+    def __init__(self, multiLandmarks, multiHandedness):
+ 
         #Angles between each finger:thumb and index, index and middle, ...
-        self.hand1BetweenFinger = []
-        self.hand2BetweenFinger = []
+        self.leftBetweenFinger = None
+        self.rightBetweenFinger = None
 
         #Angles between each knuckle of each finger, each thumb knuckle, each index knuckle, ...
-        self.hand1EachFinger = []
-        self.hand2EachFinger = []
+        self.leftEachFinger = None
+        self.rightEachFinger = None
 
         #Angles between hand and horizon [1,0,0]
-        self.hand1Horizon = -1
-        self.hand2Horizon = -1
+        self.leftHorizon = None
+        self.rightHorizon = None
         
         #Distance between hands relative to size of hands, max's at 5.
         self.handDist = MAX_HAND_DIST
     
-        if (self.noOfHands == 0):
+        if multiLandmarks is None:
+            self.noOfHands = 0
             return
-        if (self.noOfHands == 1):
-            self.hand1BetweenFinger = self.hand2BetweenFinger = self.calcBetweenFingers(multiLandmarks[0])
-            self.hand1EachFinger = self.hand2EachFinger = self.calcEachFinger(multiLandmarks[0])
-            self.hand1Horizon = self.hand2Horizon = self.calcHorizon(multiLandmarks[0])
-            return
-        if (self.noOfHands != 2):
-            raise ValueError("More than 2 hands in gesture!") 
         
+        self.noOfHands = len(multiLandmarks)
+
+        self.leftLandmark = None
+        self.rightLandmark = None
+
+        if (self.noOfHands == 1):
+            if (multiHandedness[0].classification[0].label == "left"):
+                self.leftLandmark = self.formatLandmarks(multiLandmarks[0])
+            else:
+                self.rightLandmark = self.formatLandmarks(multiLandmarks[0])
+        
+        if (self.noOfHands == 2):
+            if (multiHandedness[0].classification[0].label == "left"):
+                self.leftLandmark = self.formatLandmarks(multiLandmarks[0])
+            else:
+                self.rightLandmark = self.formatLandmarks(multiLandmarks[0])
+            if (multiHandedness[1].classification[0].label == "left"):
+                self.leftLandmark = self.formatLandmarks(multiLandmarks[1])
+            else:
+                self.rightLandmark = self.formatLandmarks(multiLandmarks[1])
+
+        if (self.noOfHands > 2):
+            raise ValueError("More than 2 hands in gesture!") 
+                
+
         #o/w must be 2 landmarked hands
-        self.hand1BetweenFinger = self.calcBetweenFingers(multiLandmarks[0])
-        self.hand2BetweenFinger = self.calcBetweenFingers(multiLandmarks[1])
-        self.hand1EachFinger = self.calcEachFinger(multiLandmarks[0])
-        self.hand2EachFinger = self.calcEachFinger(multiLandmarks[1])
-        self.hand1Horizon = self.calcHorizon(multiLandmarks[0])
-        self.hand2Horizon = self.calcHorizon(multiLandmarks[1])
-        self.handDist = self.calcHandDist(multiLandmarks[0], multiLandmarks[1])
-    
-    def getHand1Between(self, swap = False):
-        return self.hand2BetweenFinger if swap else self.hand1BetweenFinger
-    
-    def getHand2Between(self, swap = False):
-        return self.hand1BetweenFinger if swap else self.hand2BetweenFinger
-    
-    def getHand1Each(self, swap = False):
-        return self.hand2EachFinger if swap else self.hand1EachFinger
-    
-    def getHand2Each(self, swap = False):
-        return self.hand1EachFinger if swap else self.hand2EachFinger
-    
-    def getHand1Horizon(self, swap = False):
-        return self.hand2Horizon if swap else self.hand1Horizon
-    
-    def getHand2Horizon(self, swap = False):
-        return self.hand1Horizon if swap else self.hand2Horizon
-    
-    def getHandDist(self):
-        return self.handDist
+        self.leftBetweenFinger = self.calcBetweenFingers(self.leftLandmark)
+        self.rightBetweenFinger = self.calcBetweenFingers(self.rightLandmark)
+        self.leftEachFinger = self.calcEachFinger(self.leftLandmark)
+        self.rightEachFinger = self.calcEachFinger(self.rightLandmark)
+        self.leftHorizon = self.calcHorizon(self.leftLandmark)
+        self.rightHorizon = self.calcHorizon(self.rightLandmark)
+        self.handDist = self.calcHandDist(self.leftLandmark, self.rightLandmark)
 
+    def formatLandmarks(self, unformattedLandmarks):
+        return np.array([[lm.x, lm.y, lm.z] for lm in list(unformattedLandmarks.landmark)], dtype=np.float32)
 
-    def calcLMAngle(self, handLandmark, p1, p2, p3): #calculate angle between 3 landmark points
+    def calcLMAngle(self, handLandmark, p1, p2, p3): #calculate angle between 3 lareturn np.array([[lm.x, lm.y, lm.z] for lm in landmarks], dtype=np.float32)ndmark points
         vec1 = handLandmark[p1] - handLandmark[p2]
         vec2 = handLandmark[p3] - handLandmark[p2]
 
@@ -77,6 +76,8 @@ class GestureFrame:
         return np.arccos(dotProd)
     
     def calcBetweenFingers(self, handLandmark):
+        if handLandmark is None:
+            return None
         return np.array([
             #angles between each finger:
             self.calcLMAngle(handLandmark, 1,0,5),
@@ -85,7 +86,9 @@ class GestureFrame:
             self.calcLMAngle(handLandmark, 13, 0, 17)
         ])
     
-    def calcEachFinger(self, handLandmark): 
+    def calcEachFinger(self, handLandmark):
+        if handLandmark is None:
+            return None
         return np.array([
             #angles between thumb joints
             self.calcLMAngle(handLandmark, 0, 1, 2), 
@@ -114,21 +117,28 @@ class GestureFrame:
         ])
     
     def calcHorizon(self, handLandmark):
+        if handLandmark is None:
+            return None
         vec1 = handLandmark[9] - handLandmark[0] #between wrist and centre of hand
         vec2 = [1, 0 ,0] #horizon
         return self.calcVecAngle(vec1, vec2)
     
     def calcHandDist(self, handLandmark1, handLandmark2):
+        if handLandmark1 is None or handLandmark2 is None:
+            return MAX_HAND_DIST
         avHandSize = (np.linalg.norm(handLandmark1[9] - handLandmark1[0]) + np.linalg.norm(handLandmark2[9] - handLandmark2[0])) /2
         distBetween = np.linalg.norm(handLandmark1[0] - handLandmark2[0])
-        return min(distBetween / avHandSize, 5)
+        return min(distBetween / avHandSize, MAX_HAND_DIST)
+    
+    def __str__(self):
+        return ("No of hands:" + self.noOfHands + ", dist:" + self.getHandDist())
     
 
 def angleDif(a ,b):
     diff = np.abs(a - b)
     return np.minimum(diff, 2 * np.pi - diff)
 
-def dtwGestureFrameDistance(frame1, frame2):
+def dtwGestureFrameDistance(frame1, frame2, oneHandedGesture = False):
 
     weights = {
         "betweenFingers" : 0.3,
@@ -137,25 +147,41 @@ def dtwGestureFrameDistance(frame1, frame2):
         "handDist" : 0.15
     }
 
-    def frameDistance(frame1, frame2, swap):
-        if (frame1.noOfHands == 0 or frame2.noOfHands == 0):
-            return 3.14 #max val
-
-        hand1BetweenFingersDist = np.linalg.norm(angleDif(frame1.getHand1Between(),frame2.getHand1Between(swap=swap))) / len(frame1.getHand1Between())
-        hand2BetweenFingersDist = np.linalg.norm(angleDif(frame1.getHand2Between(), frame2.getHand2Between(swap=swap))) / len(frame1.getHand2Between())
-        betweenDist = (hand1BetweenFingersDist + hand2BetweenFingersDist) / 2
-
-        hand1EachFingersDist = np.linalg.norm(angleDif(frame1.getHand1Each(), frame2.getHand1Each(swap=swap))) / len(frame1.getHand1Each())
-        hand2EachFingersDist = np.linalg.norm(angleDif(frame1.getHand2Each(), frame2.getHand2Each(swap=swap))) / len(frame1.getHand2Each())
-        eachDist = (hand1EachFingersDist + hand2EachFingersDist) / 2
+    if (frame1.noOfHands == 0 or frame2.noOfHands == 0):
+        return np.pi #max val
 
 
-        hand1HorizonDist = np.linalg.norm(angleDif(frame1.getHand1Horizon(), frame2.getHand1Horizon(swap=swap)))
-        hand2HorizonDist = np.linalg.norm(angleDif(frame1.getHand2Horizon(), frame2.getHand2Horizon(swap=swap)))
-        horizonDist = (hand1HorizonDist + hand2HorizonDist) / 2
+    leftBetweenFingersDist = leftEachFingersDist = leftHorizonDist = rightBetweenFingersDist = rightEachFingersDist = rightHorizonDist = np.pi
 
-        handDist = np.abs(frame1.getHandDist() - frame2.getHandDist()) / MAX_HAND_DIST * 3.14 #Ensures handDist is between 0 and pi.
-
-        return (weights["betweenFingers"] * betweenDist + weights["eachFinger"] * eachDist + weights["horizon"] * horizonDist + weights["handDist"] * handDist)
+    if (frame1.leftLandmark is not None and frame2.leftLandmark is not None):
+        leftBetweenFingersDist = np.linalg.norm(angleDif(frame1.leftBetweenFinger, frame2.leftBetweenFinger)) / len(frame1.leftBetweenFinger)
+        leftEachFingersDist = np.linalg.norm(angleDif(frame1.leftEachFinger, frame2.leftEachFinger)) / len(frame1.leftEachFinger)
+        leftHorizonDist = np.linalg.norm(angleDif(frame1.leftHorizon, frame2.leftHorizon))
     
-    return min(frameDistance(frame1, frame2, False), frameDistance(frame1, frame2, True))
+    if (frame1.rightLandmark is not None and frame2.rightLandmark is not None):
+        rightBetweenFingersDist = np.linalg.norm(angleDif(frame1.rightBetweenFinger, frame2.rightBetweenFinger)) / len(frame1.rightBetweenFinger)
+        rightEachFingersDist = np.linalg.norm(angleDif(frame1.rightEachFinger, frame2.rightEachFinger)) / len(frame1.rightEachFinger)
+        rightHorizonDist = np.linalg.norm(angleDif(frame1.rightHorizon, frame2.rightHorizon))
+
+    if oneHandedGesture:  # If 1-handed gesture, return hand that matches best
+        betweenDist = min(leftBetweenFingersDist, rightBetweenFingersDist)
+        eachDist = min(leftEachFingersDist, rightEachFingersDist)
+        horizonDist = min(leftHorizonDist, rightHorizonDist)
+    else:  # If 2-handed gesture, average both hands
+        betweenDist = (leftBetweenFingersDist + rightBetweenFingersDist) / 2
+        eachDist = (leftEachFingersDist + rightEachFingersDist) / 2
+        horizonDist = (leftHorizonDist + rightHorizonDist) / 2
+
+    handDist = np.pi
+    if (frame1.handDist is not None and frame2.handDist is not None):
+        handDist = np.abs(frame1.handDist - frame2.handDist) / MAX_HAND_DIST * np.pi  # Ensures handDist is between 0 and pi.
+
+    print("between: " + betweenDist + ", eachDist: " + eachDist + ", horizon: " + horizonDist + "handDist:" + handDist)
+
+    return (
+        weights["betweenFingers"] * betweenDist +
+        weights["eachFinger"] * eachDist +
+        weights["horizon"] * horizonDist +
+        weights["handDist"] * handDist
+    )
+
